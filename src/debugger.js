4b24d05f32899849ed8414310896a12caa8ca797
rainyjune <dreamneverfall@gmail.com>
2014-04-07 15:27:09 +0800

    Add src/debugger

---
+++ src/debugger.js
@@ -0,0 +1,10 @@
+if (!window.console) {
+  window.console = {
+    log: function() {
+      var args = Array.prototype.slice.call(arguments);
+      var str = args.join("\n");
+      alert(str);
+    }
+  };
+}
+yuanjs.console = window.console;
