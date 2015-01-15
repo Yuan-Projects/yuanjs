3807af8fb9e5732d6ef3798fb8178610855eced5
rainyjune <rainyjune@live.cn>
2015-01-15 11:12:55 +0800

    Update src/domEvents.js

--- src/domEvents.js
+++ src/domEvents.js
@@ -73,36 +73,6 @@
     return event;
   }
 
-  (function(){
-    var cache = {},
-	guidCounter = 1,
-	expando = "data" + (new Date()).getTime();
-    
-    function getData(elem) {
-      var guid = elem[expando];
-      if (!guid) {
-	guid = elem[expando] = guidCounter++;
-	cache[guid] = {}; 
-      }
-      return cache[guid];
-    }
-
-    function removeData(elem) {
-      var guid = elem[expando];
-      if (!guid) return;
-      delete cache[guid];
-      try {
-	delete elem[expando];
-      } catch (e) {
-	if (elem.removeAttribute) {
-	  elem.removeAttribute(expando);
-	}
-      }
-    }
-
-    yuanjs.getData = getData;
-    yuanjs.removeData = removeData;
-  })();
 
   (function(){
     var nextGuid = 1;
