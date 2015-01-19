c570564bedfa39156be7353f58b9a8557d534a80
rainyjune <rainyjune@live.cn>
2015-01-19 15:50:07 +0800

    Add the width() function

--- src/css.js
+++ src/css.js
@@ -6,5 +6,20 @@
     var classRegExp = new RegExp("\\b" + className + "\\b");
     return classRegExp.test(originalClassName);
   }
+
+  function width(element, newWidth) {
+    if (newWidth) {
+      element.style.width = newWidth;
+    } else {
+      if (window.getComputedStyle) {
+	var style = window.getComputedStyle(element);
+	return style.getPropertyValue("width");
+      } else if (element.currentStyle) {
+	var currentWidth = element.currentStyle.width;
+	return currentWidth == "auto" ? element.offsetWidth : currentWidth;
+      }
+    }
+  }
   
   yuanjs.hasClass = hasClass;
+  yuanjs.width = width;
