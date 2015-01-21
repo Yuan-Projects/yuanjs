5947b5aa7c62bb3954089211b8b40b463f72842f
rainyjune <rainyjune@live.cn>
2015-01-21 16:55:39 +0800

    Update library files

--- build/yuan.js
+++ build/yuan.js
@@ -19,22 +19,23 @@
    * Setting and getting attribute values
    *
    */
-  var translations = {
-    "for": "htmlFor",
-    "class": "className",
-    "readonly": "readOnly",
-    "maxlength": "maxLength",
-    "cellspacing": "cellSpacing",
-    "rowspan": "rowSpan",
-    "colspan": "colSpan",
-    "tabindex": "tabIndex",
-    "cellpadding": "cellPadding",
-    "usemap": "useMap",
-    "frameborder": "frameBorder",
-    "contenteditable": "contentEditable"
-  };
   
   function attr(element, name, value) {
+    var translations = {
+      "for": "htmlFor",
+      "class": "className",
+      "readonly": "readOnly",
+      "maxlength": "maxLength",
+      "cellspacing": "cellSpacing",
+      "rowspan": "rowSpan",
+      "colspan": "colSpan",
+      "tabindex": "tabIndex",
+      "cellpadding": "cellPadding",
+      "usemap": "useMap",
+      "frameborder": "frameBorder",
+      "contenteditable": "contentEditable"
+    };
+    var specialAttributes = ["id", "action"];
     var property = translations[name] || name,
         propertyExists = typeof element[property] !== "undefined";
         
@@ -45,11 +46,16 @@
         element.setAttribute(name, value);
       }
     }
+
+    if (specialAttributes.indexOf(property) != -1) {
+      return element.getAttributeNode(property).nodeValue; 
+    }
     
     return propertyExists ? element[property] : element.getAttribute(name);
   }
   
   yuanjs.attr = attr;
+
   /**
    * Deferred Object
    */
