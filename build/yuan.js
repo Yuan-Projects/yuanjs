267574c070985184e44309f564f4d490534afef4
rainyjune <rainyjune@live.cn>
2015-01-23 11:23:04 +0800

    Update library files

--- build/yuan.js
+++ build/yuan.js
@@ -13,7 +13,20 @@
       }
     };
   }
+
+  function log() {
+    try {
+      console.log.apply(console, arguments);
+    } catch (e) {
+      try {
+	opera.postError.apply(opera, arguments);
+      } catch(err) {
+	alert(Array.prototype.join.call(arguments, " "));
+      }
+    }
+  }
   yuanjs.console = window.console;
+  yuanjs.log = log;
 
   /**
    * Setting and getting attribute values
