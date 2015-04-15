cdaa7516e293a355d710d829fc940567ccaa3cc6
rainyjune <rainyjune@live.cn>
2015-04-15 14:00:31 +0800

    Update src/object.js: Update namespace() function

--- src/object.js
+++ src/object.js
@@ -4,11 +4,15 @@ function extend(destination, source) {
   }
   return destination;
 }
-function namespace(str) {
+function namespace(str, value) {
   var arr = str.split("."), obj = window;
-  for ( var i = 0; i < arr.length; i++) {
-    obj[arr[i]] = obj[arr[i]] || {};
-    obj = obj[arr[i]];
+  for ( var i = 0, len = arr.length, len2 = len - 1; i < len; i++) {
+    if (i === len2) {
+      obj[arr[i]] = value;
+    } else {
+      obj[arr[i]] = obj[arr[i]] || {};
+      obj = obj[arr[i]];
+    }
   }
 }
 yuanjs.extend = extend;
