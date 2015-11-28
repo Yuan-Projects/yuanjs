a303c0cd1ff350a66171f1bd909412edc12436eb
Kang Chen <rainyjune@live.cn>
2015-11-28 12:41:09 +0800

    Update object.js

--- src/object.js
+++ src/object.js
@@ -1,9 +1,53 @@
-function extend(destination, source) {
-  for (var property in source) {
-    destination[property] = source[property];
+/**
+ * Merge multiple objects dynamically with modifying either arguments.
+ * Inspired by http://stackoverflow.com/questions/171251#16178864
+ * @example extend(obj1, obj2, ....);
+ * @return {object} The merged object.
+ */
+function extend() {
+  var result = {}, 
+      src, 
+      prop, 
+      args = getArgumentsArray(arguments),
+      toString = Object.prototype.toString;
+      
+  while (args.length > 0) {
+    src = args.shift();
+    if (toString.call(src) === "[object Object]") {
+      for (prop in src) {
+        if (src.hasOwnProperty(prop)) {
+          if (toString.call(src[prop]) == '[object Object]') {
+            result[prop] = cp(result[prop] || {}, src[prop]);
+          } else {
+            result[prop] = src[prop];
+          }
+        }
+      }
+    }
   }
-  return destination;
+  return result;
 }
+
+/**
+ *
+ * Convert function arguments to an array.
+ * The arguments object can be converted to a real Array by using:
+ * var args = Array.prototype.slice.call(arguments);
+ * But it prevents optimizations in JavaScript engines(V8 for example).
+ * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+ * @param {object} arguments The Array-like object.
+ * @return {array}
+ */
+function getArgumentsArray(arguments) {
+  var result = [],
+      len = arguments.length,
+      i;
+  for (i = 0; i < len; i++) {
+    result.push(arguments[i]);
+  }
+  return result;
+}
+
 function namespace(str, value) {
   var arr = str.split("."), obj = window;
   for ( var i = 0, len = arr.length, len2 = len - 1; i < len; i++) {
