922ee31d264a42642da7324c5cc6ae55ce45ff03
rainyjune <rainyjune@live.cn>
2016-02-19 09:37:39 +0800

    Update build files

--- build/yuan.js
+++ build/yuan.js
@@ -348,7 +348,7 @@
     return /^\s*$/.test(param);
   }
   
-  
+  // http://stackoverflow.com/a/15604206
   function replaceAll(str,mapObj) {
     var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 
@@ -886,7 +886,7 @@
           }
           readyEventHandlersInstalled = true;
         }
-      }
+      };
     })("documentReady", yuanjs);
 
     yuanjs.addEvent = addEvent;
