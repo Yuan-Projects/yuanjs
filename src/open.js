207c51101b1fb6c678182abf5cd986a6d30a346d
rainyjune <rainyjune@live.cn>
2016-02-19 11:09:32 +0800

    Add comments

--- src/open.js
+++ src/open.js
@@ -1,10 +1,12 @@
 (function (window, undefined) {
   /* jshint -W034 */
   "use strict";
+  
+  // Save the previous value of the `yuanjs` variable, so that it can be restored later on, if ｀noConflict｀ is used.
+  var previousYuanJS = window.yuanjs;
+  
   var yuanjs = {};
   yuanjs.version = "0.0.3";
-  
-  var previousYuanJS = window.yuanjs;
   
   yuanjs.noConflict = function() {
     window.yuanjs = previousYuanJS;
