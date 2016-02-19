650415ddd0027e6635415eab15218b128f3f9187
rainyjune <rainyjune@live.cn>
2016-02-19 11:04:43 +0800

    Add the noConflict mode support

--- src/open.js
+++ src/open.js
@@ -3,3 +3,10 @@
   "use strict";
   var yuanjs = {};
   yuanjs.version = "0.0.3";
+  
+  var previousYuanJS = window.yuanjs;
+  
+  yuanjs.noConflict = function() {
+    window.yuanjs = previousYuanJS;
+    return this;
+  };
