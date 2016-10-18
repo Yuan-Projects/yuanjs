574569548aae06e3dff9c9e7e74c0e65dd0bfb14
rainyjune <rainyjune@live.cn>
2016-10-18 14:36:39 +0800

    Add the urlArgs() function

--- src/helper.js
+++ src/helper.js
@@ -227,6 +227,22 @@
         return mapObj[matched.toLowerCase()];
     });
   }
+  
+  function urlArgs() {
+    var args = {};                             // Start with an empty object
+    var query = location.search.substring(1);  // Get query string, minus '?'
+    var pairs = query.split("&");              // Split at ampersands
+    for(var i = 0; i < pairs.length; i++) {    // For each fragment
+        var pos = pairs[i].indexOf('=');       // Look for "name=value"
+        if (pos == -1) continue;               // If not found, skip it
+        var name = pairs[i].substring(0,pos);  // Extract the name
+        var value = pairs[i].substring(pos+1); // Extract the value
+        name = decodeURIComponent(name);       // Decode the name
+        value = decodeURIComponent(value);     // Decode the value
+        args[name] = value;                    // Store as a property
+    }
+    return args;                               // Return the parsed arguments
+  }
 
   yuanjs.isNumber = isNumber;
   yuanjs.isString = isString;
@@ -235,3 +251,4 @@
   yuanjs.isUndefined = isUndefined;
   yuanjs.isEmpty = isEmpty;
   yuanjs.replaceAll = replaceAll;
+  yuanjs.urlArgs = urlArgs;
