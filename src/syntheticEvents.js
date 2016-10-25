9fd0ea6ad6a8abe7d489a3e001f1526ec79f6db8
rainyjune <rainyjune@live.cn>
2016-10-25 09:33:57 +0800

    Add comments

--- src/syntheticEvents.js
+++ src/syntheticEvents.js
@@ -7,6 +7,7 @@
    *        3) "scoped": A Boolean indicating whether the given event bubbles.If this value is true, deepPath will only contain a target node.
    *        4) "composed": A Boolean indicating whether the event will trigger listeners outside of a shadow root. The default is false.
    *        5) "detail": optional and defaulting to null, of type any, that is an event-dependent value associated with the event.
+   * NOTE: IE 8 and earlier require the `eventName` to be an event name that supported natively. Such as click, blur, focus...
    */
   function SyntheticEvent(eventName, params) {
     if (typeof eventName !== "string") {
@@ -20,9 +21,10 @@
       "detail": params ? params.detail : null
     };
     var event;    
+    // Use the CustomEvent interface : https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
     try {
       event = new CustomEvent(eventName, params);
-    } catch(e) {
+    } catch(e) { // The old-fashioned way
       try {
         // DOM Level 3 Events Custom event module
         event = document.createEvent('CustomEvent');
@@ -34,12 +36,12 @@
         event.detail = params.detail; // Attach the detail data to the event object
       } finally {
         if (!event) {
-          event = document.createEventObject();
-          event.type = eventName;
-          event.eventType = eventName;
+          // Internet Explorer 8 and earlier
+          event = document.createEventObject();// create event object
+          event.type = eventName;// Event type
           var detail = params.detail;
-          if (detail) {
-            for (var prop in detail) {
+          if (detail) { // Suppose it's an object
+            for (var prop in detail) { // initialize the event object
               if (detail.hasOwnProperty(prop)) {
                 event[prop] = detail[prop];
               }
