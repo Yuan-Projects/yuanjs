ae1212c151aa4cb10233def2ea95c4feaef6c0a6
rainyjune <rainyjune@live.cn>
2016-11-01 14:50:45 +0800

    Use two spaces

--- src/ajax.js
+++ src/ajax.js
@@ -1,105 +1,105 @@
-    /**
-     * Ajax request
-     *
-     */
+  /**
+   * Ajax request
+   *
+   */
 
-    function ajax(options) {
-        var dtd = Deferred();
-        var xhr = new XMLHttpRequest();
-        var url = options.url;
-        var type = options.type ? options.type.toUpperCase() : "GET";
-        var isAsyc = !!options.asyc || true;
-        var successCallBack = options.success;
-        var errorCallBack = options.error;
-        var completeCallBack = options.complete;
-        var data = options.data ? encodeFormatData(options.data) : "";
-        var dataType = options.dataType || "text";
-        var contentType = options.contentType || "application/x-www-form-urlencoded";
-        var timeout = (options.timeout && !isNaN(options.timeout) && options.timeout > 0) ? options.timeout : 0;
-        var timedout = false;
-        var headers = Object.prototype.toString.call(options.headers) === "[object Object]" ? options.headers : null;
+  function ajax(options) {
+    var dtd = Deferred();
+    var xhr = new XMLHttpRequest();
+    var url = options.url;
+    var type = options.type ? options.type.toUpperCase() : "GET";
+    var isAsyc = !!options.asyc || true;
+    var successCallBack = options.success;
+    var errorCallBack = options.error;
+    var completeCallBack = options.complete;
+    var data = options.data ? encodeFormatData(options.data) : "";
+    var dataType = options.dataType || "text";
+    var contentType = options.contentType || "application/x-www-form-urlencoded";
+    var timeout = (options.timeout && !isNaN(options.timeout) && options.timeout > 0) ? options.timeout : 0;
+    var timedout = false;
+    var headers = Object.prototype.toString.call(options.headers) === "[object Object]" ? options.headers : null;
 
-        if(timeout) {
-            var timer = setTimeout(function() {
-                timedout = true;
-                xhr.abort();
-                xhr.message = "Canceled";
-                dtd.reject(xhr);
-            },timeout);
+    if(timeout) {
+      var timer = setTimeout(function() {
+        timedout = true;
+        xhr.abort();
+        xhr.message = "Canceled";
+        dtd.reject(xhr);
+      },timeout);
+    }
+
+    if(type === "GET" && data !== "") {
+      url += (url.indexOf("?") === -1 ? "?" : "&") + data;
+    }
+    xhr.open(type, url, isAsyc);
+    if(isAsyc) {
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          callBack();
         }
+      };
+    }
 
-        if(type === "GET" && data !== "") {
-            url += (url.indexOf("?") === -1 ? "?" : "&") + data;
+    xhr.setRequestHeader("Content-Type", contentType);
+    if (headers) {
+      for (var prop in headers) {
+        if (headers.hasOwnProperty(prop)) {
+          xhr.setRequestHeader(prop, headers[prop]);
         }
-        xhr.open(type, url, isAsyc);
-        if(isAsyc) {
-          xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-              callBack();
-            }
-          };
-        }
+      }
+    }
 
-        xhr.setRequestHeader("Content-Type", contentType);
-        if (headers) {
-          for (var prop in headers) {
-            if (headers.hasOwnProperty(prop)) {
-              xhr.setRequestHeader(prop, headers[prop]);
-            }
+    switch(type) {
+      case "POST":
+        xhr.send(data);
+        break;
+      case "GET":
+        xhr.send(null);
+    }
+    if(!isAsyc) {
+      callBack();
+    }
+
+    function callBack() {
+      if(timedout){
+        return;
+      }
+      clearTimeout(timer);
+      var resultText = xhr.responseText;
+      var resultXML = xhr.responseXML;
+      var textStatus = xhr.statusText;
+      if (completeCallBack) {
+        completeCallBack(xhr, textStatus);
+      }
+      // Determine if successful
+      var status = xhr.status;
+      var isSuccess = status >= 200 && status < 300 || status === 304;
+      if(isSuccess) {
+        var resultType = xhr.getResponseHeader("Content-Type");
+        if(dataType === "xml" || (resultType && resultType.indexOf("xml") !== -1 && xhr.responseXML)){
+          if (successCallBack) {
+            successCallBack(resultXML, xhr);
+          }
+        } else if(dataType === "json" || resultType === "application/json") {
+          if (successCallBack) {
+            successCallBack(JSON.parse(resultText), xhr);
+          }
+        }else{
+          if (successCallBack) {
+            successCallBack(resultText, xhr);
           }
         }
-
-        switch(type) {
-            case "POST":
-                xhr.send(data);
-                break;
-            case "GET":
-                xhr.send(null);
+        dtd.resolve(xhr);
+      } else {
+        if (errorCallBack) {
+          errorCallBack(status, xhr);
         }
-        if(!isAsyc) {
-            callBack();
-        }
-
-        function callBack() {
-            if(timedout){
-                return;
-            }
-            clearTimeout(timer);
-            var resultText = xhr.responseText;
-            var resultXML = xhr.responseXML;
-            var textStatus = xhr.statusText;
-            if (completeCallBack) {
-              completeCallBack(xhr, textStatus);
-            }
-            // Determine if successful
-            var status = xhr.status;
-            var isSuccess = status >= 200 && status < 300 || status === 304;
-            if(isSuccess) {
-                var resultType = xhr.getResponseHeader("Content-Type");
-                if(dataType === "xml" || (resultType && resultType.indexOf("xml") !== -1 && xhr.responseXML)){
-                  if (successCallBack) {
-                    successCallBack(resultXML, xhr);
-                  }
-                } else if(dataType === "json" || resultType === "application/json") {
-                  if (successCallBack) {
-                    successCallBack(JSON.parse(resultText), xhr);
-                  }
-                }else{
-                  if (successCallBack) {
-                    successCallBack(resultText, xhr);
-                  }
-                }
-                dtd.resolve(xhr);
-            } else {
-              if (errorCallBack) {
-                errorCallBack(status, xhr);
-              }
-                dtd.reject(xhr);
-            }
-        }
-        return dtd.promise();
+        dtd.reject(xhr);
+      }
     }
-    yuanjs.ajax = ajax;
+    return dtd.promise();
+  }
+  yuanjs.ajax = ajax;
     
   // Inspired by jQuery
   function loadScript(src, successCallback, errorCallback) {
