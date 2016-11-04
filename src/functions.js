a91b11e34f3500c7f9624e335c11baf420ee7b11
rainyjune <rainyjune@live.cn>
2016-11-04 14:40:16 +0800

    Update the bind() function: add initial arguments support

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,11 @@
   yuanjs.bind = function(func, context) {
+    var slice = Array.prototype.slice; 
     if (Function.prototype.bind) {
       return Function.prototype.bind.apply(func, Array.prototype.slice.call(arguments, 1));
     }
-    var args = Array.prototype.slice.call(arguments, 2);
+    var args = slice.call(arguments, 2);
     return function() {
-      return func.apply(context, args);
+      var innerArgs = slice.call(arguments);
+      return func.apply(context, args.concat(innerArgs));
     };
   }; 
