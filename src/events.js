29e26404504928215d63503e951da92a69c10f4f
rainyjune <rainyjune@live.cn>
2017-10-11 18:16:48 +0800

    Update events.js

--- src/events.js
+++ src/events.js
@@ -1,39 +1,38 @@
-    // Events on and off
-    var Events = [];
+  // Events on and off
+  var Events = {};
 
-    function on(event, callback) {
-        if (!Events[event]) {
-            Events[event] = [];
-        }
-        Events[event].push(callback);
-        return callback;
+  function on(event, callback) {
+    if (!Events[event]) {
+      Events[event] = [];
     }
+    Events[event].push(callback);
+    return callback;
+  }
 
-    function off(event, callback) {
-        if (!Events[event]) {
-            return ;
-        }
-        if (callback) {
-            var index = Events[event].indexOf(callback);
-            if (index !== -1) {
-                Events[event].splice(index, 1);
-            }
-        } else {
-            Events[event] = [];
-        }
+  function off(event, callback) {
+    if (!Events[event]) {
+      return ;
     }
+    if (callback) {
+      var index = Events[event].indexOf(callback);
+      if (index !== -1) {
+        Events[event].splice(index, 1);
+      }
+    } else {
+      Events[event] = [];
+    }
+  }
 
-    function trigger (event) {
-        if (!Events[event]) {
-            return ;
-        }
-        var args = Array.prototype.slice.call(arguments, 1);
-        var callbackArray = Events[event];
-        for (var i = callbackArray.length - 1; i >= 0; i--) {
-            callbackArray[i].apply(callbackArray[i], args);
-        }
+  function trigger (event) {
+    if (!Events[event]) {
+      return ;
     }
-    yuanjs.on = on;
-    yuanjs.off = off;
-    yuanjs.trigger = trigger;
-    
+    var args = Array.prototype.slice.call(arguments, 1);
+    var callbackArray = Events[event];
+    for (var i = 0, len = callbackArray.length; i < len; i++) {
+      callbackArray[i].apply(callbackArray[i], args);
+    }
+  }
+  yuanjs.on = on;
+  yuanjs.off = off;
+  yuanjs.trigger = trigger;
