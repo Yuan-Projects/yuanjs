049604c0aa04ad3b2de20ff451ab517a6d9003a8
rainyjune <rainyjune@live.cn>
2019-02-27 17:18:42 +0800

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    library: "yuanjs",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx", ".css"]
+  }
+};
