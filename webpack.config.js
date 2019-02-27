0782fe69ce3498f2a4ec306d0ad2cca815c5f67d
rainyjune <rainyjune@live.cn>
2019-02-27 17:58:32 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         ],
         loader: "babel-loader",
         options: {
-          presets: ["es2015"]
+          presets: ["@babel/preset-env"]
         }
       }
     ]
