bb4a1c41f2e0421c8653a204d6d16bd4e8f164ac
rainyjune <rainyjune@live.cn>
2019-03-01 10:18:13 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     before: function(app, server) {
       // parse application/x-www-form-urlencoded
-      app.use(bodyParser.urlencoded({ extended: false }))
+      app.use(bodyParser.urlencoded({ extended: false }));
 
       app.use(bodyParser.json());
 
