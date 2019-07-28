8f907dd03a50a6e5d0721be3fad7f7e01d2fda41
rainyjune <rainyjune@live.cn>
2019-07-28 21:42:46 +0800

    Drop IE6 support

--- src/helper.js
+++ src/helper.js
@@ -28,25 +28,6 @@
    * Polyfill
    *
    */
-
-  // Emulate the XMLHttpRequest() constructor in IE5 and IE6
-  if (window.XMLHttpRequest === undefined) {
-      window.XMLHttpRequest = function() {
-          try {
-              // Use the latest version of the ActiveX object if available
-              return new ActiveXObject("Msxml2.XMLHTTP.6.0");
-          } catch (e1) {
-              try {
-                  // Otherwise fall back on an older version
-                  return new ActiveXObject("Msxml2.XMLHTTP.3.0");
-              } catch(e2) {
-                  // Otherwise, throw an error
-                  throw new Error("XMLHttpRequest is not supported");
-              }
-          }
-      };
-  }
-
   if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (searchElement /*, fromIndex */) {
       "use strict";
