1c14f7164e2ded696418a1ca8f406dda7d96ca6b
Kang Chen <rainyjune@live.cn>
2020-10-16 14:19:01 +0800

    Create escapeTextContentForBrowser.js

---
+++ src/escapeTextContentForBrowser.js
@@ -0,0 +1,25 @@
+var ESCAPE_LOOKUP = {
+  '&': '&amp;',
+  '>': '&gt;',
+  '<': '&lt;',
+  '"': '&quot;',
+  '\'': '&#x27;'
+};
+
+var ESCAPE_REGEX = /[&><"']/g;
+
+function escaper(match) {
+  return ESCAPE_LOOKUP[match];
+}
+
+/**
+ * Escapes text to prevent scripting attacks.
+ *
+ * @param {*} text Text value to escape.
+ * @return {string} An escaped string.
+ */
+function escapeTextContentForBrowser(text) {
+  return ('' + text).replace(ESCAPE_REGEX, escaper);
+}
+
+export default escapeTextContentForBrowser;
